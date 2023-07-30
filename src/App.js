1ed059aca096cb4de0605c2dc4a011311ee63f83
Moringa School <chepfaith18@gmail.com>
2023-07-30 23:02:26 +0300

    App.js-State hooks to manage different aspects of the app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import BotCollection from './components/BotCollection';
+import YourBotArmy from './components/YourBotArmy';
+import BotSpecs from './components/BotSpecs';
+import BotSort from './components/BotSort';
+import './App.css';
+
+const App = () => {
+  // State hooks to manage different aspects of the app
+  const [bots, setBots] = useState([]); // State to store bot data fetched from the API
+  const [enlistedBots, setEnlistedBots] = useState([]); // State to store enlisted bots in your army
+  const [selectedBot, setSelectedBot] = useState(null); // State to keep track of the currently selected bot for BotSpecs view
+  const [filterBy, setFilterBy] = useState(''); // State to store the selected filter class (e.g., tank, support, etc.)
+  const [sortBy, setSortBy] = useState(''); // State to store the selected sort key (health, damage, armor)
